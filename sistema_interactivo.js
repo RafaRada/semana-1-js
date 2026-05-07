c3ea2ba4a37d1b4a4a15ba1443ddb058f8366bc3
Rafael Ramos <ramos.rada.rafael@gmail.com>
2026-05-07 15:40:04 -0500

    new

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -1,18 +1,23 @@
 let nombre = prompt("Escriba su nombre: ")
 let edad = prompt("Escriba su edad: ")
-
+let validarEdad = true
 while (edad) {
 
     if (isNaN(edad)) {
         alert("Error:Por favor, ingrese una edad valida en numeros.")
+        validarEdad = false
     }
 
     else if (edad < 18) {
         alert("Hola "+nombre+", eres menor de edad.¡Sigue aprendiendo y disfrutando del código!")
+        validarEdad = false
     }
 
     else if (edad >= 18) {
-        alert("Hola "+nombre+", eres mayor de edad.¡Preparate para granes oportunidades en el mundo de la programación!")
+        alert("Hola "+nombre+", eres mayor de edad.¡Preparate para grandes oportunidades en el mundo de la programación!")
+        validarEdad = false
     }
 }
+
+
 
